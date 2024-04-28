7c03cc7903a4df6a1c250594fd0f04a7eeb0e957
luis.mendoza <l.mendozah@uniandes.edu.co>
2024-04-28 18:15:10 -0500

    refactoring login

--- app/src/main/java/com/example/vinilosmisw4203_2024/MainActivity.kt
+++ app/src/main/java/com/example/vinilosmisw4203_2024/MainActivity.kt
@@ -4,30 +4,17 @@ package com.example.vinilosmisw4203_2024
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.vinilosmisw4203_2024.ui.theme.VinilosMisw42032024Theme
 import com.example.vinilosmisw4203_2024.views.CollectionistScreen
 import com.example.vinilosmisw4203_2024.views.UserScreen
+import com.example.vinilosmisw4203_2024.views.LoginScreen
 
 
 class MainActivity : ComponentActivity() {
@@ -42,7 +29,7 @@ override fun onCreate(savedInstanceState: Bundle?) {
             ) {
                 val navController = rememberNavController()
                 NavHost(navController = navController, startDestination = "welcome") {
-                    composable("welcome") { WelcomeScreen(navController) }
+                    composable("welcome") { LoginScreen(navController) }
                     composable("User") { UserScreen() }
                     composable("Collectionist") { CollectionistScreen() }
                 }
@@ -51,53 +38,4 @@ override fun onCreate(savedInstanceState: Bundle?) {
     }
 }
 
-@Composable
-fun WelcomeScreen(navController: NavController) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.logovinilos),
-            contentDescription = "Logo Vinilos",
-            modifier = Modifier
-                .weight(1f)
-                .align(Alignment.CenterHorizontally)
-        )
-
-        Button(
-            onClick = {
-                navController.navigate("User")
-            },
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFBDBDBD)),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 8.dp)
-        ) {
-            Text(
-                text = "Login as User",
-                fontSize = 18.sp,
-                modifier = Modifier.padding(8.dp),
-                color = Color.Black
-                )
-            }
-        Button(
-            onClick = {
-                navController.navigate("collectionist")
-            },
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFBDBDBD)),
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text(
-                text = "Login as Collectionist",
-                fontSize = 18.sp,
-                modifier = Modifier.padding(8.dp),
-                color = Color.Black
-                 )
-            }
-        }
-    }
 }
