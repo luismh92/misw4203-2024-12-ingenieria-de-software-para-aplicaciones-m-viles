2007e1a59c0744c4efe62e2c166b458d678097cd
CamiloCastro <bc.castro@uniandes.edu.co>
2024-05-11 17:11:23 -0500

    Develop (#33)
    * adding components for HU03 (#15)
    
    * Feature hu03 (#19)
    
    * adding components for HU03
    
    * fixign AR
    
    * Feature hu03 (#20)
    
    * adding components for HU03
    
    * fixign AR
    
    * improving UI
    
    * Feature hu03 (#21)
    
    * adding components for HU03
    
    * fixign AR
    
    * improving UI
    
    * organizing structure
    
    * Hotfix hu03 (#22)
    
    * fixign AR
    
    * Hotfix hu03 (#23)
    
    * refactoring login
    
    * Test-userscreen (#24)
    
    * Hotfix hu03 (#25)
    
    * adding components for HU03
    
    * fixign AR
    
    * improving UI
    
    * organizing structure
    
    * fixing AR
    
    * refactoring login
    
    * fixing test
    
    * Hotfix hu03 (#26)
    
    * fixing AR
    
    * Hotfix hu03 (#27)
    
    * fixing test for artist
    
    * adding test for album (#28)
    
    * Feature/hu 02 (#32)
    
    * Update README.md
    
    Added instructions
    
    * Develop (#29)
    
    * adding components for HU03 (#15)
    
    * Feature hu03 (#19)
    
    * adding components for HU03
    
    * fixign AR
    
    * Feature hu03 (#20)
    
    * adding components for HU03
    
    * fixign AR
    
    * improving UI
    
    * Feature hu03 (#21)
    
    * adding components for HU03
    
    * fixign AR
    
    * improving UI
    
    * organizing structure
    
    * Hotfix hu03 (#22)
    
    * fixign AR
    
    * Hotfix hu03 (#23)
    
    * refactoring login
    
    * Test-userscreen (#24)
    
    * Hotfix hu03 (#25)
    
    * adding components for HU03
    
    * fixign AR
    
    * improving UI
    
    * organizing structure
    
    * fixing AR
    
    * refactoring login
    
    * fixing test
    
    * Hotfix hu03 (#26)
    
    * fixing AR
    
    * Hotfix hu03 (#27)
    
    * fixing test for artist
    
    * adding test for album (#28)
    
    ---------
    
    Co-authored-by: Christian Camilo Peña <cc.pena2@uniandes.edu.co>
    
    * modal finalizado album
    
    ---------
    
    Co-authored-by: Hernán Martínez <hd.martinezd1@uniandes.edu.co>
    Co-authored-by: luismh92 <l.mendozah@uniandes.edu.co>
    Co-authored-by: Christian Camilo Peña <cc.pena2@uniandes.edu.co>
    
    ---------
    
    Co-authored-by: luismh92 <l.mendozah@uniandes.edu.co>
    Co-authored-by: Christian Camilo Peña <cc.pena2@uniandes.edu.co>
    Co-authored-by: Hernán Martínez <hd.martinezd1@uniandes.edu.co>

--- app/src/main/java/com/example/vinilosmisw4203_2024/views/AlbumListScreen.kt
+++ app/src/main/java/com/example/vinilosmisw4203_2024/views/AlbumListScreen.kt
@@ -1,26 +1,20 @@
 package com.example.vinilosmisw4203_2024.views
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
@@ -29,16 +23,19 @@ import com.example.vinilosmisw4203_2024.viewsModels.AlbumViewModel
 
 @Composable
 fun AlbumListScreen(viewModel: AlbumViewModel) {
+    var showDetailDialog by remember { mutableStateOf(false) }
+    var selectedAlbum by remember { mutableStateOf<Album?>(null) }
+
     val loading = remember { mutableStateOf(true) }
     val error = remember { mutableStateOf("") }
-    val albums by viewModel.artists.observeAsState()
+    val albums by viewModel.albums.observeAsState()
 
     LaunchedEffect(Unit) {
         try {
             viewModel.fetchAlbums()
             loading.value = false
         } catch (e: Exception) {
-            error.value = e.message ?: "Unknown error"
+            error.value = e.message ?: "Error desconocido"
             loading.value = false
         }
     }
@@ -48,44 +45,88 @@ fun AlbumListScreen(viewModel: AlbumViewModel) {
     } else if (error.value.isNotEmpty()) {
         Text("Error: ${error.value}")
     } else {
-        albums?.let { AlbumList(it) }
-    }
-}
-
-@Composable
-fun AlbumList(albums: List<Album>) {
-    Column {
-        Text(text = "12 VINILOS",
-            modifier = Modifier.padding(8.dp).align(Alignment.CenterHorizontally),
-            fontWeight = FontWeight.Bold, fontSize = 30.sp)
-        LazyColumn {
-            items(albums) { album ->
-                AlbumItem(album)
+        albums?.let {
+            AlbumList(it) { album ->
+                selectedAlbum = album
+                showDetailDialog = true
             }
         }
     }
 
+    selectedAlbum?.let {
+        if (showDetailDialog) {
+            AlbumDetailDialog(album = it, onDismissRequest = { showDetailDialog = false })
+        }
+    }
 }
 
 @Composable
-fun AlbumItem(album: Album) {
-    Card(modifier = Modifier.padding(8.dp)) {
+fun AlbumList(albums: List<Album>, onAlbumClick: (Album) -> Unit) {
+    Column {
+        Text(
+            text = "12 VINILOS",
+            modifier = Modifier.padding(8.dp).align(Alignment.CenterHorizontally),
+            fontSize = 30.sp
+        )
+        LazyColumn {
+            items(albums) { album ->
+                AlbumItem(album, onAlbumClick)
+            }
+        }
+    }
+}
+
+@Composable
+fun AlbumItem(album: Album, onAlbumClick: (Album) -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .clickable { onAlbumClick(album) },
+        elevation = 4.dp
+    ) {
         Column(modifier = Modifier.padding(16.dp)) {
             Image(
-                painter = rememberImagePainter(
-                    data = album.cover,
-                    builder = {
-                        crossfade(true)
-                    }
-                ),
-                contentDescription = "Album Cover",
+                painter = rememberImagePainter(data = album.cover),
+                contentDescription = "Portada del Álbum",
                 modifier = Modifier
                     .height(150.dp)
                     .fillMaxWidth(),
-                contentScale = ContentScale.Crop // Ajusta cómo se ajusta la imagen dentro de su contenedor
+                contentScale = ContentScale.Crop
             )
-            Text(album.name, style = MaterialTheme.typography.h6)
-            Text(album.genre, style = MaterialTheme.typography.body2) // Ejemplo de cómo añadir más datos
+            Text(album.name, fontSize = 18.sp)
+            Text(album.genre, fontSize = 14.sp)
         }
     }
+}
+
+@Composable
+fun AlbumDetailDialog(album: Album, onDismissRequest: () -> Unit) {
+    AlertDialog(
+        onDismissRequest = { onDismissRequest() },
+        title = {
+            Text(text = album.name)
+        },
+        text = {
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Image(
+                    painter = rememberImagePainter(album.cover),
+                    contentDescription = "Portada del Álbum",
+                    modifier = Modifier
+                        .height(200.dp)
+                        .fillMaxWidth(),
+                    contentScale = ContentScale.Crop
+                )
+                Text("Género: ${album.genre}")
+                Text("Fecha de lanzamiento: ${album.releaseDate}")
+                Text("Descripción: ${album.description}")
+                Text("Sello discográfico: ${album.recordLabel}")
+            }
+        },
+        confirmButton = {
+            Button(onClick = { onDismissRequest() }) {
+                Text("Cerrar")
+            }
+        }
+    )
 }
