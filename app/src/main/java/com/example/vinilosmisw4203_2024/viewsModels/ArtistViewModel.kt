9c689b4825c7499d3433093e627664353fc53818
CamiloCastro <bc.castro@uniandes.edu.co>
2024-05-12 23:18:29 -0500

    ANRs implementado con viewModelScope.launch(Dispatchers.IO) (#41)

--- app/src/main/java/com/example/vinilosmisw4203_2024/viewsModels/ArtistViewModel.kt
+++ app/src/main/java/com/example/vinilosmisw4203_2024/viewsModels/ArtistViewModel.kt
@@ -1,30 +1,30 @@
 package com.example.vinilosmisw4203_2024.viewsModels
+
 import android.util.Log
-import androidx.lifecycle.ViewModel
-import com.example.vinilosmisw4203_2024.models.Artist
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.vinilosmisw4203_2024.models.Artist
 import com.example.vinilosmisw4203_2024.repositories.ArtistRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class ArtistViewModel : ViewModel() {
-
-    private val respository = ArtistRepository()
+    private val repository = ArtistRepository()
     private val _artists = MutableLiveData<List<Artist>>()
     val artists: LiveData<List<Artist>> = _artists
 
-    fun fetchArtist() {
-        viewModelScope.launch {
+    fun fetchArtists() {
+        _artists.value = listOf() // Pre-clear the current artist list
+        viewModelScope.launch(Dispatchers.IO) { // IMPLEMANTACION DE ANRs UTILIZANDO VIEWMODELSCOPE.LAUNCH(DISPATCHERS.IO)
             try {
-                val listArtist = respository.getArtists()
-                _artists.value = listArtist
-            }catch (e: Exception){
-                Log.d("Repo", "fetch Artist exception ${e.message}")
+                val listArtists = repository.getArtists()
+                _artists.postValue(listArtists)
+            } catch (e: Exception) {
+                Log.d("ArtistViewModel", "fetch Artists exception: ${e.message}")
+                _artists.postValue(emptyList()) // Manejo de errores postando una lista vacía
             }
         }
     }
 }
-
-
-
