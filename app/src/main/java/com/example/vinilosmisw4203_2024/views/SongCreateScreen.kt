e7820f735f985492e94c2820ceb5f648b2bc18b4
luismh92 <l.mendozah@uniandes.edu.co>
2024-05-19 22:25:01 -0500

    Feature hu04 (#43)
    * adding new feature for create album
    
    * adding new feature for create album
    
    * adding new feature for create album
    
    * adding new feature for create album

---
+++ app/src/main/java/com/example/vinilosmisw4203_2024/views/SongCreateScreen.kt
@@ -0,0 +1,13 @@
+package com.example.vinilosmisw4203_2024.views
+
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SongCreateScreen(){
+    Box {
+        Text("Creating song")
+    }
+}
