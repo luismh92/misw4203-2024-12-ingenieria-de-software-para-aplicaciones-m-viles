40eba469c7a7ec3fbef34d67f6d1204274ce458d
luis.mendoza <l.mendozah@uniandes.edu.co>
2024-05-19 22:23:52 -0500

    adding new feature for create album

--- app/src/main/java/com/example/vinilosmisw4203_2024/views/AlbumListScreen.kt
+++ app/src/main/java/com/example/vinilosmisw4203_2024/views/AlbumListScreen.kt
@@ -1,28 +1,20 @@
 package com.example.vinilosmisw4203_2024.views
 
-import android.graphics.Paint.Align
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.LargeFloatingActionButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
